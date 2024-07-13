0f09c82a80ec2c90bb97793bfce9180e099a202f
JooHyunJeong <joycekr.dev@gmail.com>
2024-07-13 20:47:26 +1000

    코지 코더 vue 3 강좌 적용

---
+++ my-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="name">
+      {{ name }}
+    </div>
+    <button 
+      class="btn btn-primary"
+      v-on:click="updateName">
+        Click
+      </button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup(){
+    //name 바꾸는 함수가 적용되기 위해서 ref를 사용
+    let name = ref('Fridge Master');
+
+    // const greeting = (name) => {
+    //   return 'Hello, ' + name;
+    // };
+
+    const updateName = () => {
+      //ref를 사용하면 name 뒤에 .value를 붙여야 한다
+      name.value = 'cofdjfke';
+      console.log('타냐?');
+    };
+
+    return{
+      name,
+      updateName,
+      
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
