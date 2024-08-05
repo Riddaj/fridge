a5338ccead2e2374d77f074e8623973c6f1d358d
JooHyunJeong <angridda@gmail.com>
2024-08-05 22:35:50 +1000

    050824 app.vue 업데이트(to do list 강좌 적용)

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,12 +1,39 @@
 <template>
-    <div class="name">
-      {{ name }}
-    </div>
-    <button 
-      class="btn btn-primary"
-      v-on:click="updateName">
-        Click
-      </button>
+  <div v-show="toggle">true</div>
+  <div v-show="!toggle">false</div>
+  <button @click="onToggle">Toggle</button>
+  <div class="container">
+    <h2>TO DO LIST</h2>
+    <form 
+      @submit.prevent="onSubmit"
+      class="d-flex">
+      
+      <div class="flex-grow-1 mr-2">
+        <!-- 데이터 바인딩, oninput event -->
+        <input v-bind:type="type" 
+          v-model="todo"
+          placeholder="Type new to-do"
+        > 
+      </div>
+      <div>
+        <button 
+          class="btn btn-primary"
+          type="submit">
+            Add
+          </button>
+        </div>
+      </form>
+      <!-- todos 리스트 출력 -->
+      <!-- {{ todos }} -->
+      <div
+        v-for="t in todos"
+        :key="t.id"
+        class="card mt-2">
+        <div class="card-body p-2">
+            {{ t.subject }}
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -14,22 +41,49 @@ import { ref } from 'vue';
 export default {
   setup(){
     //name 바꾸는 함수가 적용되기 위해서 ref를 사용
-    let name = ref('Fridge Master');
+   const todo = ref('');
+   //toggole 선언
+   const toggle = ref(false);
+   //to do list 만들기(list니까 array로 놓음)
+   const todos = ref([
+    {id:1, subject: 'chicken breast'},
+    {id: 2, subject: 'mango'}
+   ]);
+
+   const type = ref('text');
 
     // const greeting = (name) => {
     //   return 'Hello, ' + name;
     // };
 
-    const updateName = () => {
+    const onSubmit = () => {
+      //form에서 리로딩하는 것을 막기 위한 것
+      //event.prevnetDefault();
       //ref를 사용하면 name 뒤에 .value를 붙여야 한다
-      name.value = 'cofdjfke';
-      console.log('타냐?');
+      console.log(todo.value)
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value
+      });
+      
     };
 
+    const onToggle = () =>{
+      toggle.value = !toggle.value;
+    };
+
+    // const updateName = (e) => {
+    //   name.value - e.target.value;
+    //   console.log(e.target.value)
+    // };
+
     return{
-      name,
-      updateName,
-      
+      todo,
+      todos,
+      onSubmit,
+      type,
+      toggle,
+      onToggle,
     };
   }
 }
