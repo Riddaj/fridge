849acdd63437527d116e663fe9116131e732691a
JooHyunJeong <angridda@gmail.com>
2024-08-06 18:44:31 +1000

    060824 App.vue update

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -4,33 +4,40 @@
   <button @click="onToggle">Toggle</button>
   <div class="container">
     <h2>TO DO LIST</h2>
-    <form 
-      @submit.prevent="onSubmit"
-      class="d-flex">
-      
-      <div class="flex-grow-1 mr-2">
-        <!-- 데이터 바인딩, oninput event -->
-        <input v-bind:type="type" 
-          v-model="todo"
-          placeholder="Type new to-do"
-        > 
-      </div>
-      <div>
-        <button 
-          class="btn btn-primary"
-          type="submit">
-            Add
-          </button>
-        </div>
-      </form>
+    <TodoSimpleForm/>
+
       <!-- todos 리스트 출력 -->
       <!-- {{ todos }} -->
+      <div v-if="!todos.length">
+        추가된 To do가 없습니다
+      </div>
       <div
-        v-for="t in todos"
+        v-for="(t, index) in todos"
         :key="t.id"
         class="card mt-2">
-        <div class="card-body p-2">
-            {{ t.subject }}
+
+        <div class="card-body p-2 d-flex align-items-center">
+          <div class="form-check flex-grow-1">
+            <input 
+              class="form-check-input" 
+              type="checkbox"
+              v-model="t.completed"
+            >
+            <label 
+              class="form-check-label"
+              :class="{ todo: t.completed }"
+            >
+              {{ t.subject }}
+            </label>
+          </div>
+          <div>
+            <button 
+              class="btn btn-danger btn-sm"
+              @click="deleteTodo(index)"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
   </div>
@@ -38,19 +45,25 @@
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+
 export default {
+  components: { TodoSimpleForm },
+  component: {
+    TodoSimpleForm
+  },
   setup(){
     //name 바꾸는 함수가 적용되기 위해서 ref를 사용
    const todo = ref('');
    //toggole 선언
    const toggle = ref(false);
    //to do list 만들기(list니까 array로 놓음)
-   const todos = ref([
-    {id:1, subject: 'chicken breast'},
-    {id: 2, subject: 'watermelon'}
-   ]);
-
-   const type = ref('text');
+   const todos = ref([]);
+   const hasError = ref(false);
+   const todoStyle = {
+    textDecoration: 'line-through',
+    color: 'gray'
+   };
 
     // const greeting = (name) => {
     //   return 'Hello, ' + name;
@@ -61,11 +74,17 @@ export default {
       //event.prevnetDefault();
       //ref를 사용하면 name 뒤에 .value를 붙여야 한다
       console.log(todo.value)
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value
-      });
-      
+      if(todo.value === ''){
+        hasError.value = true;
+      }else{
+          todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,   //체크박스를 위한 completed
+        });
+        hasError.value = false;
+        todo.value = '';
+      }
     };
 
     const onToggle = () =>{
@@ -77,18 +96,29 @@ export default {
     //   console.log(e.target.value)
     // };
 
+    //to do list 삭제
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+      console.log('delete Todo')
+    };
+
     return{
       todo,
       todos,
       onSubmit,
-      type,
       toggle,
       onToggle,
+      hasError,
+      todoStyle,
+      deleteTodo,
     };
   }
 }
 </script>
 
 <style>
-
+  .todo{
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
